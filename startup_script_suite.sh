5e5149846ef95187907f93ebb7eb8d12291d1169
josschavezf <joselynchavezf@gmail.com>
2022-10-31 01:56:49 -0400

    update github link

--- startup_script_suite.sh
+++ startup_script_suite.sh
@@ -2,7 +2,7 @@
 
 
 R -e "install.packages('remotes')"
-R -e "remotes::install_github('drieslab/Giotto')"
+R -e "remotes::install_github('drieslab/Giotto@suite')"
 
 pip install --upgrade igraph
 pip install --upgrade leidenalg
